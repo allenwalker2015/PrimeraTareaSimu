ae47963f6bca0a40d44f869503818f28221ffb35
AllenAP <00053812@uca.edu.sv>
2019-04-03 10:00:39 -0600

    Se modifico la L para LocalBs y LocalKs

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
-//Archivo que contiene el procedimiento principal de la implementación,
-//describiendo los pasos a seguir para la aplicación del FEM al problema
+//Archivo que contiene el procedimiento principal de la implementaciï¿½n,
+//describiendo los pasos a seguir para la aplicaciï¿½n del FEM al problema
 //de transferencia de calor.
 
 #include <iostream>
@@ -15,52 +15,54 @@ int main()
     vector<Matrix> localKs;
     vector<Vector> localbs;
 
-    //Se preparan también las variables para la K y la b globales, y una para las incógnitas de
-    //temperatura, que es donde se almacenará la respuesta.
+    //Se preparan tambiï¿½n las variables para la K y la b globales, y una para las incï¿½gnitas de
+    //temperatura, que es donde se almacenarï¿½ la respuesta.
     Matrix K;
     Vector b;
     Vector T;
 
-    //Se coloca primero una introducción con todas las características de la implementación
-    cout << "IMPLEMENTACI"<<char(224)<<"N DEL M"<<char(144)<<"TODO DE LOS ELEMENTOS FINITOS\n"
-         << "\t- TRANSFERENCIA DE CALOR\n" << "\t- 1 DIMENSI"<<char(224)<<"N\n"
-         << "\t- FUNCIONES DE FORMA LINEALES\n" << "\t- PESOS DE GALERKIN\n"
+    //Se coloca primero una introducciï¿½n con todas las caracterï¿½sticas de la implementaciï¿½n
+    cout << "IMPLEMENTACI" << char(224) << "N DEL M" << char(144) << "TODO DE LOS ELEMENTOS FINITOS\n"
+         << "\t- TRANSFERENCIA DE CALOR\n"
+         << "\t- 1 DIMENSI" << char(224) << "N\n"
+         << "\t- FUNCIONES DE FORMA LINEALES\n"
+         << "\t- PESOS DE GALERKIN\n"
          << "*********************************************************************************\n\n";
 
-    //Se crea un objeto mesh, que contendrá toda la información de la malla
+    //Se crea un objeto mesh, que contendrï¿½ toda la informaciï¿½n de la malla
     mesh m;
-    //Se procede a obtener toda la información de la malla y almacenarla en m
+    //Se procede a obtener toda la informaciï¿½n de la malla y almacenarla en m
     leerMallayCondiciones(m);
 
     //Se procede a crear la K local y la b local de cada elemento, almacenando estas
     //estructuras en los vectores localKs y localbs
-    crearSistemasLocales(m,localKs,localbs);
-    //Descomentar la siguiente línea para observar las Ks y bs creadas
+    crearSistemasLocales(m, localKs, localbs);
+    //Descomentar la siguiente lï¿½nea para observar las Ks y bs creadas
     //showKs(localKs); showbs(localbs);
 
     //Se inicializan con ceros la K global y la b global
-    zeroes(K,m.getSize(NODES));
-    zeroes(b,m.getSize(NODES));
+    zeroes(K, m.getSize(NODES));
+    zeroes(b, m.getSize(NODES));
     //Se procede al proceso de ensamblaje
-    ensamblaje(m,localKs,localbs,K,b);
-    //Descomentar la siguiente línea para observar las estructuras ensambladas
+    ensamblaje(m, localKs, localbs, K, b);
+    //Descomentar la siguiente lï¿½nea para observar las estructuras ensambladas
     //showMatrix(K); showVector(b);
 
     //Se aplican primero las condiciones de contorno de Neumann
-    applyNeumann(m,b);
-    //Descomentar la siguiente línea para observar los cambios en b
+    applyNeumann(m, b);
+    //Descomentar la siguiente lï¿½nea para observar los cambios en b
     //showVector(b);
 
     //Luego se aplican las condiciones de contorno de Dirichlet
-    applyDirichlet(m,K,b);
-    //Descomentar la siguiente línea para observar el SEL final luego
+    applyDirichlet(m, K, b);
+    //Descomentar la siguiente lï¿½nea para observar el SEL final luego
     //de los cambios provocados por Dirichlet
     //showMatrix(K); showVector(b);
 
-    //Se prepara con ceros el vector T que contendrá las respuestas
-    zeroes(T,b.size());
+    //Se prepara con ceros el vector T que contendrï¿½ las respuestas
+    zeroes(T, b.size());
     //Finalmente, se procede a resolver el SEL
-    calculate(K,b,T);
+    calculate(K, b, T);
 
     //Se informa la respuesta:
     cout << "La respuesta es: \n";
--- sel.h
+++ sel.h
@@ -1,119 +1,142 @@
-//La función recibe:
+//La funciï¿½n recibe:
 //- Una matriz
-//La función muestra en pantalla el contenida de la matrix, fila por fila
-void showMatrix(Matrix K){
-    for(int i=0;i<K.at(0).size();i++){
+//La funciï¿½n muestra en pantalla el contenida de la matrix, fila por fila
+void showMatrix(Matrix K)
+{
+    for (int i = 0; i < K.at(0).size(); i++)
+    {
         cout << "[\t";
-        for(int j=0;j<K.size();j++){
+        for (int j = 0; j < K.size(); j++)
+        {
             cout << K.at(i).at(j) << "\t";
         }
         cout << "]\n";
     }
 }
 
-//La función recibe un arreglo de matrices, y las muestra
+//La funciï¿½n recibe un arreglo de matrices, y las muestra
 //en pantalla una por una
-void showKs(vector<Matrix> Ks){
-    for(int i=0;i<Ks.size();i++){
-        cout << "K del elemento "<< i+1 << ":\n";
+void showKs(vector<Matrix> Ks)
+{
+    for (int i = 0; i < Ks.size(); i++)
+    {
+        cout << "K del elemento " << i + 1 << ":\n";
         showMatrix(Ks.at(i));
         cout << "*************************************\n";
     }
 }
 
-//La función recibe:
+//La funciï¿½n recibe:
 //- Un vector
-//La función asume que recibe un vector columna y muestra su contenido
+//La funciï¿½n asume que recibe un vector columna y muestra su contenido
 //en pantalla en una sola fila
-void showVector(Vector b){
+void showVector(Vector b)
+{
     cout << "[\t";
-    for(int i=0;i<b.size();i++){
+    for (int i = 0; i < b.size(); i++)
+    {
         cout << b.at(i) << "\t";
     }
     cout << "]\n";
 }
 
-//La función recibe un arreglo de vectores, y los muestra
+//La funciï¿½n recibe un arreglo de vectores, y los muestra
 //en pantalla uno por uno
-void showbs(vector<Vector> bs){
-    for(int i=0;i<bs.size();i++){
-        cout << "b del elemento "<< i+1 << ":\n";
+void showbs(vector<Vector> bs)
+{
+    for (int i = 0; i < bs.size(); i++)
+    {
+        cout << "b del elemento " << i + 1 << ":\n";
         showVector(bs.at(i));
         cout << "*************************************\n";
     }
 }
 
-//La función recibe:
+//La funciï¿½n recibe:
 //- Un elemento
 //- El objeto mesh
-//La función construye la matrix local K para el elemento
-//especificado de acuerdo a la formulación del problema
-Matrix createLocalK(int element,mesh &m){
+//La funciï¿½n construye la matrix local K para el elemento
+//especificado de acuerdo a la formulaciï¿½n del problema
+Matrix createLocalK(int element, mesh &m)
+{
     //Se prepara la matriz y sus dos filas (se sabe que es una matriz 2x2)
     Matrix K;
     Vector row1, row2;
 
-    //De acuerdo a la formulación, la matriz local K tiene la forma:
+    //De acuerdo a la formulaciï¿½n, la matriz local K tiene la forma:
     //          (k/l)*[ 1 -1 ; -1 1 ]
 
+    float l = m.getElements()[element].getNode2() - m.getElements()[element].getNode1();
+    cout<<"Local B => La L es:"<<l;
     //Se extraen del objeto mesh los valores de k y l
-    float k = m.getParameter(THERMAL_CONDUCTIVITY), l = m.getParameter(ELEMENT_LENGTH);
+    float k = m.getParameter(THERMAL_CONDUCTIVITY);
     //Se crean las filas
-    row1.push_back(k/l); row1.push_back(-k/l);
-    row2.push_back(-k/l); row2.push_back(k/l);
+    row1.push_back(k / l);
+    row1.push_back(-k / l);
+    row2.push_back(-k / l);
+    row2.push_back(k / l);
     //Se insertan las filas en la matriz
-    K.push_back(row1); K.push_back(row2);
+    K.push_back(row1);
+    K.push_back(row2);
 
     return K;
 }
 
-//La función recibe:
+//La funciï¿½n recibe:
 //- Un elemento
 //- El objeto mesh
-//La función construye el vector local b para el elemento
-//especificado de acuerdo a la formulación del problema
-Vector createLocalb(int element,mesh &m){
-    //Se prepara el vector b (se sabe que será un vector 2x1)
+//La funciï¿½n construye el vector local b para el elemento
+//especificado de acuerdo a la formulaciï¿½n del problema
+Vector createLocalb(int element, mesh &m)
+{
+    //Se prepara el vector b (se sabe que serï¿½ un vector 2x1)
     Vector b;
 
-    //Se sabe que el vector local b tendrá la forma:
+    //Se sabe que el vector local b tendrï¿½ la forma:
     //          (Q*l/2)*[ 1 ; 1 ]
 
     //Se extraen del objeto mesh los valores de Q y l
-    float Q = m.getParameter(HEAT_SOURCE), l = m.getParameter(ELEMENT_LENGTH);
+    float l = m.getElements()[element].getNode2() - m.getElements()[element].getNode1();
+    cout<<"Local B => La L es:"<<l;
+
+    float Q = m.getParameter(HEAT_SOURCE);
     //Se insertan los datos en el vector
-    b.push_back(Q*l/2); b.push_back(Q*l/2);
+    b.push_back(Q * l / 2);
+    b.push_back(Q * l / 2);
 
     return b;
 }
 
-//La función recibe:
+//La funciï¿½n recibe:
 //- El objeto mesh
 //- Un arreglo de matrices
 //- Un arreglo de vectores
-//La función construye una K y una b locales para cada elemento de la malla,
+//La funciï¿½n construye una K y una b locales para cada elemento de la malla,
 //y los almacena en su arreglo correspondiente
-void crearSistemasLocales(mesh &m,vector<Matrix> &localKs,vector<Vector> &localbs){
+void crearSistemasLocales(mesh &m, vector<Matrix> &localKs, vector<Vector> &localbs)
+{
     //Se recorren los elementos
-    for(int i=0;i<m.getSize(ELEMENTS);i++){
+    for (int i = 0; i < m.getSize(ELEMENTS); i++)
+    {
         //Por cada elemento, se crea su K y su b
-        localKs.push_back(createLocalK(i,m));
-        localbs.push_back(createLocalb(i,m));
+        localKs.push_back(createLocalK(i, m));
+        localbs.push_back(createLocalb(i, m));
     }
 }
 
-//La función recibe:
+//La funciï¿½n recibe:
 //- El elemento actual
 //- La matriz local K
 //- La matriz global K
-//La función inserta la K local en la K global de acuerdo a los nodos
+//La funciï¿½n inserta la K local en la K global de acuerdo a los nodos
 //del elemento
-void assemblyK(element e,Matrix localK,Matrix &K){
-    //Se determinan los nodos del elemento actual como los índices de la K global
+void assemblyK(element e, Matrix localK, Matrix &K)
+{
+    //Se determinan los nodos del elemento actual como los ï¿½ndices de la K global
     int index1 = e.getNode1() - 1;
     int index2 = e.getNode2() - 1;
 
-    //Se utilizan los índices para definir las celdas de la submatriz
+    //Se utilizan los ï¿½ndices para definir las celdas de la submatriz
     //a la que se agrega la matriz local del elemento actual
     K.at(index1).at(index1) += localK.at(0).at(0);
     K.at(index1).at(index2) += localK.at(0).at(1);
@@ -121,102 +144,111 @@ void assemblyK(element e,Matrix localK,Matrix &K){
     K.at(index2).at(index2) += localK.at(1).at(1);
 }
 
-//La función recibe:
+//La funciï¿½n recibe:
 //- El elemento actual
 //- El vector local b
 //- El vector global b
-//La función inserta la b local en la b global de acuerdo a los nodos
+//La funciï¿½n inserta la b local en la b global de acuerdo a los nodos
 //del elemento
-void assemblyb(element e,Vector localb,Vector &b){
-    //Se determinan los nodos del elemento actual como los índices de la b global
+void assemblyb(element e, Vector localb, Vector &b)
+{
+    //Se determinan los nodos del elemento actual como los ï¿½ndices de la b global
     int index1 = e.getNode1() - 1;
     int index2 = e.getNode2() - 1;
 
-    //Se utilizan los índices para definir las celdas del subvector
+    //Se utilizan los ï¿½ndices para definir las celdas del subvector
     //al que se agrega el vector local del elemento actual
     b.at(index1) += localb.at(0);
     b.at(index2) += localb.at(1);
 }
 
-//La función recibe:
+//La funciï¿½n recibe:
 //- El objeto mesh
 //- El arreglo de Ks locales
 //- El arreglo de bs locales
 //- La matriz K global
 //- El vector b global
-//La función se encarga de ensamblar adecuadamente todos los sistemas locales en
+//La funciï¿½n se encarga de ensamblar adecuadamente todos los sistemas locales en
 //la K y la b globales
-void ensamblaje(mesh &m,vector<Matrix> &localKs,vector<Vector> &localbs,Matrix &K,Vector &b){
+void ensamblaje(mesh &m, vector<Matrix> &localKs, vector<Vector> &localbs, Matrix &K, Vector &b)
+{
     //Se recorren todos los elementos de la malla, uno por uno
-    for(int i=0;i<m.getSize(ELEMENTS);i++){
+    for (int i = 0; i < m.getSize(ELEMENTS); i++)
+    {
         //Se extrae del objeto mesh el elemento actual
         element e = m.getElement(i);
         //Se ensamblan la K y la b del elemento actual en las variables globales
-        assemblyK(e,localKs.at(i),K);
-        assemblyb(e,localbs.at(i),b);
+        assemblyK(e, localKs.at(i), K);
+        assemblyb(e, localbs.at(i), b);
     }
 }
 
-//La función recibe:
+//La funciï¿½n recibe:
 //- El objeto mesh
 //- El vector b global
-//La función aplica en la b global las condiciones de Neumann en las
+//La funciï¿½n aplica en la b global las condiciones de Neumann en las
 //posiciones que correspondan
-void applyNeumann(mesh &m,Vector &b){
+void applyNeumann(mesh &m, Vector &b)
+{
     //Se recorren las condiciones de Neumann, una por una
-    for(int i=0;i<m.getSize(NEUMANN);i++){
-        //Se extrae la condición de Neumann actual
-        condition c = m.getCondition(i,NEUMANN);
-        //En la posición de b indicada por el nodo de la condición,
-        //se agrega el valor indicado por la condición
-        b.at(c.getNode1()-1) += c.getValue();
+    for (int i = 0; i < m.getSize(NEUMANN); i++)
+    {
+        //Se extrae la condiciï¿½n de Neumann actual
+        condition c = m.getCondition(i, NEUMANN);
+        //En la posiciï¿½n de b indicada por el nodo de la condiciï¿½n,
+        //se agrega el valor indicado por la condiciï¿½n
+        b.at(c.getNode1() - 1) += c.getValue();
     }
 }
 
-//La función recibe:
+//La funciï¿½n recibe:
 //- El objeto mesh
 //- La matriz K global
 //- El vector b global
-//La función aplica en la K y b globales las condiciones de Dirichlet, eliminando
+//La funciï¿½n aplica en la K y b globales las condiciones de Dirichlet, eliminando
 //las filas correspondientes, y enviando desde el lado izquierdo del SEL al lado
 //derecho los valores de las columnas correspondientes
-void applyDirichlet(mesh &m,Matrix &K,Vector &b){
+void applyDirichlet(mesh &m, Matrix &K, Vector &b)
+{
     //Se recorren las condiciones de Dirichlet, una por una
-    for(int i=0;i<m.getSize(DIRICHLET);i++){
-        //Se extrae la condición de Dirichlet actual
-        condition c = m.getCondition(i,DIRICHLET);
-        //Se establece el nodo de la condición como el índice
-        //para K y b globales donde habrá modificaciones
-        int index = c.getNode1()-1;
+    for (int i = 0; i < m.getSize(DIRICHLET); i++)
+    {
+        //Se extrae la condiciï¿½n de Dirichlet actual
+        condition c = m.getCondition(i, DIRICHLET);
+        //Se establece el nodo de la condiciï¿½n como el ï¿½ndice
+        //para K y b globales donde habrï¿½ modificaciones
+        int index = c.getNode1() - 1;
 
-        //Se elimina la fila correspondiente al nodo de la condición
-        K.erase(K.begin()+index); //Se usa un iterator a la posición inicial, y se
-        b.erase(b.begin()+index); //le agrega la posición de interés
+        //Se elimina la fila correspondiente al nodo de la condiciï¿½n
+        K.erase(K.begin() + index); //Se usa un iterator a la posiciï¿½n inicial, y se
+        b.erase(b.begin() + index); //le agrega la posiciï¿½n de interï¿½s
 
         //Se recorren las filas restantes, una por una, de modo que
         //el dato correspondiente en cada fila a la columna del nodo de la
-        //condición, se multiplique por el valor de Dirichlet, y se envíe al
+        //condiciï¿½n, se multiplique por el valor de Dirichlet, y se envï¿½e al
         //lado derecho del SEL con su signo cambiado
-        for(int row=0;row<K.size();row++){
+        for (int row = 0; row < K.size(); row++)
+        {
             //Se extrae el valor ubicado en la columna correspondiente
-            //al nodo de la condición
+            //al nodo de la condiciï¿½n
             float cell = K.at(row).at(index);
             //Se elimina la columna correspondiente
-            //al nodo de la condición
-            K.at(row).erase(K.at(row).begin()+index);
-            //El valor extraído se multiplica por el valor de la condición,
+            //al nodo de la condiciï¿½n
+            K.at(row).erase(K.at(row).begin() + index);
+            //El valor extraï¿½do se multiplica por el valor de la condiciï¿½n,
             //se le cambia el signo, y se agrega al lado derecho del SEL
-            b.at(row) += -1*c.getValue()*cell;
+            b.at(row) += -1 * c.getValue() * cell;
         }
     }
 }
 
-//La función recibe:
+//La funciï¿½n recibe:
 //- La matriz K global
 //- El vector b global
-//- El vector T que contendrá los valores de las incógnitas
-//La función se encarga de resolver el SEL del problema
-void calculate(Matrix &K, Vector &b, Vector &T){
+//- El vector T que contendrï¿½ los valores de las incï¿½gnitas
+//La funciï¿½n se encarga de resolver el SEL del problema
+void calculate(Matrix &K, Vector &b, Vector &T)
+{
     //Se utiliza lo siguiente:
     //      K*T = b
     // (K^-1)*K*T = (K^-1)*b
@@ -226,7 +258,7 @@ void calculate(Matrix &K, Vector &b, Vector &T){
     //Se prepara la inversa de K
     Matrix Kinv;
     //Se calcula la inversa de K
-    inverseMatrix(K,Kinv);
+    inverseMatrix(K, Kinv);
     //Se multiplica la inversa de K por b, y el resultado se almacena en T
-    productMatrixVector(Kinv,b,T);
+    productMatrixVector(Kinv, b, T);
 }
